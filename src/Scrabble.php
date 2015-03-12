1784a4ae491660ff81cf756ae68412fe49b46e34
chitra atmakuru & Abeer Khakwani <student@epicodus.com>
2015-03-12 10:54:21 -0700

    Add array that holds letters that are worth 2 points. Add else if statment that checks if the letter is worht two points then adds 2 to the counter

--- src/Scrabble.php
+++ src/Scrabble.php
@@ -6,13 +6,20 @@ class Scrabble {
           $split_array_input= str_split( $string);
           $adder= 0;
           $one_point_array = array("a","e","i","o","u","l","s","n","r","t");
+          $two_point_array = array("d","g");
+          $three_point_array = array("b","c","m","p");
+
          foreach ($split_array_input as $letter)
          {
               if (in_array($letter,$one_point_array))
               {
+                  $adder += 1;
 
+              }
+              elseif (in_array($letter,$two_point_array))
+              {
 
-                 $adder += 1;
+                 $adder += 2;
 
               }
 
