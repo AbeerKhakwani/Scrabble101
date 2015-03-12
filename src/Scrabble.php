dfbbdeca783df8dec8536a496bc55db2d2e63112
chitra atmakuru & Abeer Khakwani <student@epicodus.com>
2015-03-12 10:43:18 -0700

    Test to see if 2 letters are input it adds the points and return number of points;

--- src/Scrabble.php
+++ src/Scrabble.php
@@ -7,14 +7,16 @@ class Scrabble {
           $adder= 0;
           $one_point_array = array("a","e","i","o","u","l","s","n","r","t");
          foreach ($split_array_input as $letter)
-         {         
+         {
               if (in_array($letter,$one_point_array))
               {
 
 
-                 $adder.= 1;
+                 $adder += 1;
 
               }
+
+
          }
 
        return $adder;
