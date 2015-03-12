18fd49ef80d85b1958cd66e1e721d719fd75c7f4
chitra atmakuru & Abeer Khakwani <student@epicodus.com>
2015-03-12 09:59:01 -0700

    Start Test One for Spec One

---
+++ tests/ScrabbleTest.php
@@ -0,0 +1,29 @@
+<?php
+
+
+   require_once "src/Scrabble.php";
+
+
+
+
+    class ScrabbleTest extends PHPUnit_Framework_TestCase
+    {
+
+
+        function test_scrabble_letter()
+        {
+            $test_scrabble = new Scrabble;
+            $input = "A";
+            
+        }
+
+
+
+
+
+
+    }
+
+
+
+?>
