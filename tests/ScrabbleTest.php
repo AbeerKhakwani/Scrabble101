27936f0aab398cccf375e24dc0724ed48dec3224
chitra atmakuru & Abeer Khakwani <student@epicodus.com>
2015-03-12 10:02:55 -0700

    Finish Writing test 1

--- tests/ScrabbleTest.php
+++ tests/ScrabbleTest.php
@@ -11,10 +11,16 @@
 
 
         function test_scrabble_letter()
-        {
+        {   //Arrange
             $test_scrabble = new Scrabble;
             $input = "A";
-            
+            //Act
+            $result = $test_scrabble->ScrabbleChecker($input);
+
+            //Assert
+            $this->assertEquals(1,$result);
+
+
         }
 
 
